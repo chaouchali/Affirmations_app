b300b05bc07b2cf9317407d58216d696b75ac835
ali chaouch <amjilorbs@gmail.com>
2021-11-01 11:20:26 +0100

    Final Commit

---
+++ app/src/main/java/com/ali_chaouch/affirmations/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.ali_chaouch.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
